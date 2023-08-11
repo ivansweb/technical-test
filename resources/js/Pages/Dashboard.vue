a5f278e220947e3b632a90befb8416a390c3a2e3
Ivan Pereira Decskill <134951809+ivansweb@users.noreply.github.com>
2023-08-11 23:36:35 +0100

    Installing Inertia, UI Icons and creating main layout

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>hello world</h1>
+    {{ laravelVersion }}
+
+</template>
+
+<script>
+
+
+  export default {
+    name: 'Welcome',
+
+    props: {
+      'laravelVersion': String
+    }
+  }
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 require('./bootstrap');
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { InertiaProgress } from '@inertiajs/progress';
+import Layout from './Shared/LayoutDefault.vue';
+
+createInertiaApp({
+    resolve: name => {
+     let page = require(`./Pages/${name}`).default;
+     page.layout = Layout;
+     return page;
+    },
+
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+});
+
+InertiaProgress.init({ color: '#4B5563' });
